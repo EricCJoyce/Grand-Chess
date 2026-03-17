5d2415c5fc88412d0f496536cf815650e5aeca4c
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2026-03-17 12:19:35 -0400

    Add files via upload

--- gamestate.h
+++ gamestate.h
@@ -2331,8 +2331,6 @@ unsigned char isWin(GameState* gs)
       }
     else if(wMatNonK == 0 && bMatNonK == 0)                         //  Game is over if only Kings remain
       return GAME_OVER_STALEMATE;
-    else if(gs->moveCtr == 50)                                      //  Game is over if moveCtr reaches 50.
-      return GAME_OVER_STALEMATE;
 
     return GAME_ONGOING;
   }
